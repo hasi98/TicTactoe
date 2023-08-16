4f391ea988b757b08d20205dc7d125da75965ee5
Hasith Lakshan <whlmuthukumara@gmail.com>
2023-08-16 12:02:07 +0530

    added winner logic and show status message to the user

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,31 +1,7 @@
-import { useState } from 'react'
+
 import Square from "./Square"
 
-const Board = () => {
-
-const [squares, setSquares] = useState(Array(9).fill(null));
-
-const [isXNext, setIsXNext] = useState(false);
-
-const handleSquareClick = (clickedPosition) => {
-
-  if(squares[clickedPosition]) {
-    return;
-  }
-
-
-  setSquares((currentSquares) => {
-
-    return currentSquares.map((squareValue, position) => {
-      if(clickedPosition === position) {
-        return isXNext ? "X" : "0"
-      }
-
-      return squareValue;
-    })
-  })
-  setIsXNext((currentIsXNext) => !currentIsXNext)
-};
+const Board = ({ squares, handleSquareClick }) => {
 
 const renderSqaure = (position) => {
   return <Square value={squares[position]} onClick={() => handleSquareClick(position)}/>
